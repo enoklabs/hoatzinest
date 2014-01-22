d64b68315c93301ee8f4e154fcac100a91b60695
Enok Madrid <enokslaboratory@gmail.com>
2014-01-22 10:16:27 -0800

    optimized sign in form
    added link to sign up

--- application/views/login.php
+++ application/views/login.php
@@ -4,8 +4,6 @@
     <div class="row">
         <div class="large-6 large-centered columns formpadding">
             <h4 class="form-title greyDark">Sign In</h4>
-            <br/>
-            <style> .errors {color: red;}</style>
             <?php echo validation_errors();?>
             <?php echo form_open('login/validate_credentials');?>
             <br/>
@@ -17,11 +15,12 @@
             </div>
 
             <div class="row">
-                <div class="large-12  columns">
-                    <div>
-                        <button a class="button secondary radius wide" type=submit>Sign In</button>
-                    </div>
-            </div>
+                <div class="large-4 columns">
+                    <button a class="button secondary radius wide" type=submit>Sign In</button>
+                </div>
+                <div class="large-8 columns right">
+                    <p class="right">Don't have an account? <a href="<?php echo base_url();?>login/signup">&nbsp;Register Here</a></p>
+                </div>
         <?php echo form_close();?>
         </div>
     </div>
