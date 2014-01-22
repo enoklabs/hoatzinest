9d26eb950ebed93d2a82161d493512cb758ac302
Enok Madrid <enokslaboratory@gmail.com>
2014-01-21 16:37:18 -0800

    Update showcase.php
    quick update

--- application/views/showcase.php
+++ application/views/showcase.php
@@ -3,10 +3,16 @@
     <div class="row">
         <div class="large-12 columns">
             <h3>HoatziNest Hall of Fame!</h3>
+            <p>The most successful projects that got started here in the HoatziNest</p>
             
             <div class="row main">
-                <p>The most successful projects that got started here in the HoatziNest</p><br/><br/>
-                <p><h4>There aren't any completed projects yet - will yours be the first?</h4></p>
+
+               <!-- TEMPORARY MESSAGE - NO PROJECTS TO DISPLAY -->
+                <div class="large-8 columns panel radius large-centered text-center pad-top">
+                    <blockquote class="pad-top"><br/>There aren't any completed projects yet - will yours be the first?<br/><br/></blockquote>
+                </div>
+
+
             </div>
         </div> <!-- End Projects -->
     </div>
